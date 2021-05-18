875fdecc61fd8a0569ac66f187a0586fcfd15fa7
gsaidheeraj <60067496+GSaiDheeraj@users.noreply.github.com>
2021-05-18 22:57:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,58 @@
 from flask import Flask,render_template, url_for , redirect
-
+#from forms import RegistrationForm, LoginForm
+#from sklearn.externals import joblib
 from flask import request
 import numpy as np
-
+from PIL import Image
+from flask import flash
+#from flask_sqlalchemy import SQLAlchemy
+#from model_class import DiabetesCheck, CancerCheck
 
 
 import os
-import keras
-import tensorflow as tf
+from tensorflow import keras
 import tensorflow
 from tensorflow.keras.models import load_model
-#from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing import image
 
 from flask import send_from_directory
-# from keras.preprocessing import image
-# import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
 
 #from this import SQLAlchemy
 app=Flask(__name__,template_folder='template')
 
 
-
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
-
+# UPLOAD_FOLDER = dir_path + '/uploads'
+# STATIC_FOLDER = dir_path + '/static'
 UPLOAD_FOLDER = 'uploads'
 STATIC_FOLDER = 'static'
 #from keras.models import load_model
 
 # global graph
 # graph = tf.get_default_graph()
-model =load_model('Covid_model.h5')
+model = load_model('Covid_model.h5')
 
-# call model to predict an image
+
 def api(full_path):
-#     with graph.as_default():
     data = tensorflow.keras.preprocessing.image.load_img(full_path, target_size=(224, 224, 3))
+    #print(data.shape)
     data = np.expand_dims(data, axis=0)
-    data = data/ 255
+    data = data * 1.0 / 255
+
+    #with graph.as_default():
     predicted = model.predict(data)
     return predicted
 
 
 # procesing uploaded file and predict it
+	
 @app.route('/upload', methods=['POST','GET'])
 def upload_file():
-#     with graph.as_default():
 
     if request.method == 'GET':
         return render_template('index.html')
@@ -67,9 +71,11 @@ def upload_file():
 
             return render_template('predict.html', image_file_name = file.filename, label = label, accuracy = accuracy)
         except :
-            flash("Please select the image first !!", "danger")
+            flash("Please select the image first !!", "danger")      
             return redirect(url_for("corona"))
+	
 
+	
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
